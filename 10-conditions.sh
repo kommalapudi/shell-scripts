fdfba1e15a9f86740345e36d79d59078f78bdc1c
chaitanya <kommalkrishna@gmail.com>
2026-06-19 18:11:08 +0530

    update new variables file

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -11,5 +11,5 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "Given number $NUMBER is greater than 20."
 else
-    echo "Given number $NUMBER is not less than 20."
+    echo "Given number $NUMBER is not greater than 20."
 fi

b82d3dc548c87751be4429437b90b3b5c5dbc860
chaitanya <kommalkrishna@gmail.com>
2026-06-19 17:43:58 +0530

    update new variables file

---
+++ 09-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FRUITS=("apple" "banana" "cherry" "date")
+echo "All fruits: ${FRUITS[@]}"
+echo "First fruit: ${FRUITS[0]}"
+echo "Second fruit: ${FRUITS[1]}"
+echo "Third fruit: ${FRUITS[2]}"
+echo "Fourth fruit: ${FRUITS[3]}"

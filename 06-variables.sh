a13e27d893ddfc812d1f08185850a44516fd2779
chaitanya <kommalkrishna@gmail.com>
2026-06-19 16:05:02 +0530

    update new variables file

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-TIMESTANP=$(date)
+STARTTIME=$(date)
 
-echo "Script executed at: $TIMESTANP"
+echo "Script executed at: $STARTTIME"
+
+sleep 5
+
+ENDTIME=$(date)
+
+echo "Script completed at: $ENDTIME"
+
+TOTALTIME=$(( $(date -d "$ENDTIME" +%s) - $(date -d "$STARTTIME" +%s) ))
+echo "Total execution time: $TOTALTIME seconds"
+

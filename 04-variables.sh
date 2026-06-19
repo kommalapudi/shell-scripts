9a44154ef52143166582f7a850cab513a3a54c64
chaitanya <kommalkrishna@gmail.com>
2026-06-19 15:35:01 +0530

    update new variables file

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1 #No space between variable name and equal sign
+PERSON2=$2
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1, I'm fine, thanks for asking!"
+echo "$PERSON1:: that's great to hear. What have you been up to lately?"
+echo "$PERSON2:: I've been busy with work, but I managed to take a short vacation last week."

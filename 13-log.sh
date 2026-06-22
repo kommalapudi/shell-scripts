8d1b68ca54904ff38c0bb4ca81efad983bed0098
chaitanya <kommalkrishna@gmail.com>
2026-06-22 15:57:02 +0530

    update new variables file

---
+++ 13-log.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripts"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 failed."
+        exit 1
+    else
+        echo "$2 succeeded."
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "mysql"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "nodejs"

4a27924afa21c34a9c58225dabc1b9b4b56d4308
chaitanya <kommalkrishna@gmail.com>
2026-06-22 17:46:32 +0530

    update new variables file

---
+++ 15-loops.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+#for i in {1..5}; do
+#    echo "Iteration $i"
+#done
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripts"
+LOGS_FILE="$LOGS_FOLDER/$(basename "$0").log"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Please run as root" | tee -a "$LOGS_FILE"
+    exit 1
+fi
+
+mkdir -p "$LOGS_FOLDER"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 failed." | tee -a "$LOGS_FILE"
+        exit 1
+    else
+        echo "$2 succeeded." | tee -a "$LOGS_FILE"
+    fi
+}
+
+for package in $@; do   # sudo sh 14-loops.sh nginx mysql nodejs
+    dnf install "$package" -y &>> "$LOGS_FILE"
+    if [ $? -ne 0 ]; then
+        echo "$package failed not installed, installing now"
+        exit 1
+    else
+        echo "$package already installed, skipping installation"
+    fi
+    VALIDATE $? "$package"
+done

eb84a5785bc94f65a59cd6b933cde67eb8cfa30e
chaitanya <kommalkrishna@gmail.com>
2026-06-22 13:19:21 +0530

    update new variables file

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-dnf install -y nginx
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+echo "Installing nginx web server..."
+dnf install nginx -y
+
+if [ $? -eq 0 ]; then
+  echo "nginx installed successfully."
+else
+  echo "Failed to install nginx."
+  exit 1
+fi
+
+dnf install mysql -y
+if [ $? -eq 0 ]; then
+  echo "mysql installed successfully."
+else
+  echo "Failed to install mysql."
+  exit 1
+fi
+
+dnf install nodejs -y
+if [ $? -eq 0 ]; then
+  echo "nodejs installed successfully."
+else
+  echo "Failed to install nodejs."
+  exit 1
+fi

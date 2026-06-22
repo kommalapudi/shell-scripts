956b536e51982dd4460eec71f08bd10fa2c55a17
chaitanya <kommalkrishna@gmail.com>
2026-06-22 16:21:29 +0530

    update new variables file

--- 13-log.sh
+++ 13-log.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/shell-scripts"
 LOGS_FILE="$LOGS_FOLDER/$(basename "$0").log"
 
 if [ "$USERID" -ne 0 ]; then
-    echo "Please run as root"
+    echo "Please run as root" | tee -a "$LOGS_FILE"
     exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p "$LOGS_FOLDER"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 failed."
+        echo "$2 failed." | tee -a "$LOGS_FILE"
         exit 1
     else
-        echo "$2 succeeded."
+        echo "$2 succeeded." | tee -a "$LOGS_FILE"
     fi
 }
 

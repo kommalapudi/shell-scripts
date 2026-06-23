dbcf1c4e6e0c53f9a9e24c8cf27339a29ad38164
chaitanya <kommalkrishna@gmail.com>
2026-06-23 11:57:51 +0530

    update new variables file

---
+++ 19-trap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e 
+echo "hello world"
+echo " I am learning Linux"
+echoo "This is a typo, it will cause an error and exit the script"
+echo "This will not be printed because of the error above"

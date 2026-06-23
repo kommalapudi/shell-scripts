3ef67b3814c65939e9d6ff26222bc31e6d80bb27
chaitanya <kommalkrishna@gmail.com>
2026-06-23 12:10:38 +0530

    update new variables file

--- 19-trap.sh
+++ 19-trap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e 
+set -e #ERR
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+#trap 'echo "Error: Script failed at line $LINENO with exit code $?"' ERR
 echo "hello world"
 echo " I am learning Linux"
 echoo "This is a typo, it will cause an error and exit the script"

b7bcfee0bb39d583403b17302282762ba0400872
chaitanya <kommalkrishna@gmail.com>
2026-06-28 15:26:55 +0530

    update note

--- 21.script1.sh
+++ 21.script1.sh
@@ -6,3 +6,8 @@ echo "I am in: $COUNTRY"
 echo "PID of SCRIPT-1: $$"
 
 sh 22-script2.sh
+source 22-script2.sh
+
+#note:
+#sh script-2.sh -> seperate process, cant access the script-1 variables
+#source script-2.sh -> same process, can access the script-1 variables
